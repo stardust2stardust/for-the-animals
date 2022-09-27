3ce22b69d1cf6329246d78e787eedeaebe97c112
Jeni Hoyt <ziahrahl@gmail.com>
2022-09-27 11:27:14 -0400

    add style sheet and burger menu icon;

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import MobileMenuIcon from "./MobileMenuIcon";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <MobileMenuIcon />
+      <h1>Logo here</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/MobileMenuIcon.js
@@ -0,0 +1,14 @@
+import menuIcon from "../images/burger-menu-icon.svg";
+
+const MobileMenuIcon = () => {
+  return (
+    <div className="mobile-menu-icon">
+      <img
+        src={menuIcon}
+        alt="Your SVG"
+      />
+    </div>
+  );
+};
+
+export default MobileMenuIcon;
