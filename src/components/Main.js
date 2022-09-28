4becbd26c97291701e7b5f546cbab24ed55c07a6
Jeni Hoyt <ziahrahl@gmail.com>
2022-09-28 16:00:08 -0400

    install axios and create pseudo db file to work with

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react";
 import MainSection from "./MainSection";
+import axios from "axios";
 
 const Main = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/events").then((response) => {
+      console.log("promise fulfilled");
+      setEvents(response.data);
+    });
+  }, []);
+  console.log("render", events.length, "events");
+  // axios.get("http://localhost:3001/events").then((response) => {
+  //   const events = response.data;
+  //   console.log(events);
+  // });
+
   return (
     <main>
       <MainSection />
